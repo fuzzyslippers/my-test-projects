a444541bf2604e88f2ed103a42538b9dc72e3883
Nick Ippoliti <fuzzy@parsix-desktop.(none)>
2009-01-25 03:25:49 +0000

    added a guess the number game
    Known bugs in guess the number:
    	will randomly crash, don't know how to fix it, if someone knows please tell me or 			fix the code. thanks

---
+++ guessthenumber.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import random
+import urllib
+
+print "Let's play 'Guess the number'"
+print "You win if you guess the number in 10 turns or less"
+number = random.randint(1,1000)
+guesses = input("Please insert a number: ")
+turn = 0
+print 'Current guess: ' + str(guesses)
+
+while guesses < number:
+	print "Number is greater than current guess."
+	guesses = input("Please insert a number greater than current guess: ")
+	turn += 1
+while guesses > number:
+	print "Number is less than current guess."
+	guesses = input("Please insert a number less than current guess: ")
+	turn += 1
+while guesses is 100 > number:
+	print "Number is much less than current guess."
+	guesses = input("Please insert a number less than current guess: ")
+	turn += 1 
+while guesses is 100 < number:
+	print "Number is much more than current guess."
+	guesses = input("Please insert a number more than current guess: ")
+	turn += 1 
+
+if guesses == number:
+	if turn < 10:
+		print "You win!!! You guessed the number in " + str(turn) +" turns"
+		exit()
+
+	if turn == 10:
+		print "You win!!! You guessed the number in " + str(turn) +" turns"
+		exit()
+
+	if turn > 10:
+		print "You lose... You guessed the number in " + str(turn) +" turns"
+		exit()
